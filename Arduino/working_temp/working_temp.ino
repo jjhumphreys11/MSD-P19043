54a8669cfd94f1f110efc24f7dd523423c3b3fa4
Jay Humphreys (RIT Student) <jmh1476@ad.rit.edu>
2019-03-22 17:09:12 -0400

    single_transmission not fully working.  working_temp is for testing

--- Arduino/working_temp/working_temp.ino
+++ Arduino/working_temp/working_temp.ino
@@ -3,7 +3,6 @@
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
    
-Adafruit_BNO055 bno1 = Adafruit_BNO055(55, BNO055_ADDRESS_A); //Wrist Sensor
 Adafruit_BNO055 bno2 = Adafruit_BNO055(56, BNO055_ADDRESS_B); //Hand Sensor
 char mode = 0;
 char t = 0;
@@ -51,42 +50,10 @@ void setup()
       //Serial.print("No BNO055 detected ... Check your wiring or I2C ADDR!");
     }
   }
-  setCal();       // Set Calibration Values for Sensor 1 (Wrist)
-  setCal_2();      // Set Calibration Values for Sensor 2 (Hand)
-  bno1.setExtCrystalUse(true);  
+      // Set Calibration Values for Sensor 1 (Wrist)
+  setCal_2();      // Set Calibration Values for Sensor 2 (Hand)  
   bno2.setExtCrystalUse(true);
   StartTime = 1;
-}
-
-//Hard coding the Calibration Data, do this after you take the Calibration Values, pg 48 of datasheet to learn how to do this
-// or just search the Adafruit forums, this is done for both sensors
-void setCal()
-{
-  byte calData;
-  bno1.setMode( bno1.OPERATION_MODE_CONFIG );    // Put into CONFIG_Mode
-  calData = bno1.setCalvalARL(232);
-  calData = bno1.setCalvalARM(3);
-  calData = bno1.setCalvalMRL(254);
-  calData = bno1.setCalvalMRM(2);
-  calData = bno1.setCalvalAOXL(19);
-  calData = bno1.setCalvalAOXM(0);
-  calData = bno1.setCalvalAOYL(7);
-  calData = bno1.setCalvalAOYM(0);
-  calData = bno1.setCalvalAOZL(19);
-  calData = bno1.setCalvalAOZM(0);
-  calData = bno1.setCalvalMOXL(175);
-  calData = bno1.setCalvalMOXM(255);
-  calData = bno1.setCalvalMOYL(220);
-  calData = bno1.setCalvalMOYM(0);
-  calData = bno1.setCalvalMOZL(129);
-  calData = bno1.setCalvalMOZM(255);
-  calData = bno1.setCalvalGOXL(254);
-  calData = bno1.setCalvalGOXM(255);
-  calData = bno1.setCalvalGOYL(255);
-  calData = bno1.setCalvalGOYM(255);
-  calData = bno1.setCalvalGOZL(0);
-  calData = bno1.setCalvalGOZM(0);
-  bno1.setMode( bno1.OPERATION_MODE_NDOF );    // Put into NDOF Mode
 }
 
 void setCal_2()
@@ -139,8 +106,8 @@ void loop(void)
       StartTime = millis();
       while(Serial1.read() != 'D')
       {
-        acc1 = bno1.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
-        gyro1 = bno1.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+        acc1 = bno2.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+        gyro1 = bno2.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
         acc2 = bno2.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
         gyro2 = bno2.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
         CurrentTime = (millis()-StartTime)/1000.0;
@@ -166,8 +133,8 @@ void loop(void)
       StartTime = millis();
       while(Serial1.read() != 'D')
       {
-        acc1 = bno1.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
-        gyro1 = bno1.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+        acc1 = bno2.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+        gyro1 = bno2.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
         acc2 = bno2.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
         gyro2 = bno2.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
         CurrentTime = (millis()-StartTime)/1000.0;
@@ -188,8 +155,8 @@ void loop(void)
       StartTime = millis();
       while(Serial1.read() != 'D')
       {
-        acc1 = bno1.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL); //Get an array that has lin acc values for sensor 1
-        gyro1 = bno1.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE); //Get an array that has ang acc values for sensor 1
+        acc1 = bno2.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL); //Get an array that has lin acc values for sensor 1
+        gyro1 = bno2.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE); //Get an array that has ang acc values for sensor 1
         CurrentTime = (millis()-StartTime)/1000.0;
         Serial1.print(
           (CurrentTime, 3) + "," 
@@ -210,11 +177,12 @@ void loop(void)
       StartTime = millis();
       while(Serial.read() != 'D')
       {
-        acc1 = bno1.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
-        gyro1 = bno1.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+        acc1 = bno2.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+        gyro1 = bno2.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
         acc2 = bno2.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
         gyro2 = bno2.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
         CurrentTime = (millis()-StartTime)/1000.0;
+        //Serial.println("working R.");
         Serial.print(
           (CurrentTime, 3) + "," 
           + String(acc1.x(), 2) + "," + String(acc1.y(), 2) + "," + String(acc1.z(), 2) + ","
